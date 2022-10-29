6a61d9c4391c2d27a868d6c839e6ba2bd5f6adf3
Paul <paul@Pauls-MacBook-Air.local>
2022-10-29 12:21:34 +0700

    add files for inheritance

---
+++ Work/4_10/stock.py
@@ -0,0 +1,21 @@
+
+class Stock:
+    """
+    An instance of a stock holding, consisting of name, shares and price.
+    """
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares 
+        self.price = price 
+
+    def cost(self):
+        """
+        return cost as shares * price
+        """
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        """
+        sell a number of shares
+        """
+        self.shares -= nshares
