1871c02157c2de15f9ed5caa38b67ab56ff0c738
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-07-31 11:54:14 +0700

    modified stock.py

--- Work/5_1/stock.py
+++ Work/5_1/stock.py
@@ -24,4 +24,40 @@ class Stock:
         return f'Stock({self.name}, {self.shares}, {self.price})'
 
 
-        
+# How inheritance works
+
+# Reading Attributes with Single Inheritance
+class A: pass
+class B(A): pass
+class C(A): pass
+class D(B): pass
+class E(D): pass
+
+print(B.__bases__)
+
+# Method Reolution Order or MRO
+
+print(E.__mro__)
+
+# MRO in Multiple Inheritance
+
+class F: pass 
+class G: pass 
+class H(F, G): pass 
+class I(G): pass 
+class J(H, I): pass
+
+print(H.__mro__)
+print(I.__mro__)
+
+# MIXIN Pattern
+
+class Loud:
+    def noise(self):
+        return super().noise().upper()
+
+print(Loud.__mro__)
+print(Loud.__bases__)
+
+
+# Exercises 5.1 - 5.5
